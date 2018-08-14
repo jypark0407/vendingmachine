49d598af3a6dee2106665937995fe7d31b7ced1d
Jinyoung Park <jypark04070@gmail.com>
2018-08-14 16:50:43 +0900

    create class VendingMachine

--- test_vm.py
+++ test_vm.py
@@ -1,25 +1,21 @@
-from vm import run, init
+from vm import VendingMachine
 
 def test_initial_change_should_be_zero():
-    init()
-    assert "잔액은 0원입니다" == run("잔액")
+    m = VendingMachine() # 자판기의 instance를 만들어서 m이라는 변수에 assign.
+    assert "잔액은 0원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
-    init()
-    assert "100원을 넣었습니다" ==  run("동전 100")
-    assert "잔액은 100원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 
 def test_accumulation_of_change():
-    init()
-    run ("동전 100")
-    run ("동전 100")
-    assert "잔액은 200원입니다" == run("잔액")
+    m = VendingMachine()
+    m.run ("동전 100")
+    m.run ("동전 100")
+    assert "잔액은 200원입니다" == m.run("잔액")
 
 def test_cannot_read_command():
-    init()
-    assert "알 수 없는 명령입니다" == run("아무거나")
-#
-# def test_cannot_read_command_2():
-#     init()
-#     assert "알 수 없는 명령어입니다" == run("잔액 반환")
+    m = VendingMachine()
+    assert "알 수 없는 명령입니다" == m.run("아무거나")
