d1ee35a5dc091c9e8fa34621cb83df3da261fe55
Jinyoung Park <jypark04070@gmail.com>
2018-08-20 12:05:16 +0900

    add additional feature

--- test_vm.py
+++ test_vm.py
@@ -7,8 +7,6 @@ def test_initial_change_should_be_zero():
 def test_insert_coin_and_check():
     m = VendingMachine()
     assert "100원을 넣었습니다" == m.run("동전 100")
-    assert "잔액은 100원입니다" == m.run("잔액")
-
 
 def test_accumulation_of_change():
     m = VendingMachine()
@@ -16,6 +14,23 @@ def test_accumulation_of_change():
     m.run ("동전 100")
     assert "잔액은 200원입니다" == m.run("잔액")
 
-def test_cannot_read_command():
+def test_unknown_command():
     m = VendingMachine()
     assert "알 수 없는 명령입니다" == m.run("아무거나")
+
+def test_check_leftover_charge():
+    m = VendingMachine()
+    assert "잔액은 100원입니다" == m.run("잔액")
+
+def test_insert_coin_and_check():
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
+
+def test_enough_change():
+    m = VendingMachine()
+    assert "커피가 나왔습니다" == m.run("동전 100")
+
+def test_not_enough_change():
+    m = VendingMachine()
+    assert "잔액이 부족합니다" == m.run("동전 100")
--- vm.py
+++ vm.py
@@ -8,9 +8,20 @@ class VendingMachine:
 
         if cmd == "잔액":
             return "잔액은 " + str(self._change) + "원입니다"
+
         elif cmd == "동전":
             coin = params[0]
-            self._change += int(params[0])
+            self._change+= int(coin)
             return coin + "원을 넣었습니다"
+
+        elif cmd =="음료":
+            if self._change >= 150:
+                self._change = self._change - 150
+                return "커피가 나왔습니다"
+            else:
+                return "잔액이 부족합니다"
+            # coin = params[0]
+            # self._change += int(params[0])
+            # return coin + "원을 넣었습니다"
         else:
             return "알 수 없는 명령입니다"
