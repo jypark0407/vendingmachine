370f09228fb8ab75c67ba958ecffb6babe9e7551
Jinyoung Park <jypark04070@gmail.com>
2018-08-20 23:36:55 +0900

    add files

--- test_vm.py
+++ test_vm.py
@@ -1,36 +1,56 @@
 from vm import VendingMachine
 
+
 def test_initial_change_should_be_zero():
-    m = VendingMachine() # 자판기의 instance를 만들어서 m이라는 변수에 assign.
+    m = VendingMachine()
     assert "잔액은 0원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
     m = VendingMachine()
     assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 def test_accumulation_of_change():
     m = VendingMachine()
-    m.run ("동전 100")
-    m.run ("동전 100")
+    m.run("동전 100")
+    m.run("동전 100")
     assert "잔액은 200원입니다" == m.run("잔액")
+
+def test_음료_뽑기():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "커피가 나왔습니다" == m.run("음료 커피")
+    assert "잔액은 350원입니다" == m.run("잔액")
+
+def test_모르는_음료_뽑기():
+    m = VendingMachine()
+    m.run("동전 500")
+    assert "알 수 없는 음료입니다" == m.run("음료 맥주")
+    assert "잔액은 500원입니다" == m.run("잔액")
+
+def test_동전이_부족한_상황에서_음료_뽑기():
+    m = VendingMachine()
+    m.run("동전 100")
+    assert "잔액이 부족합니다" == m.run("음료 커피")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 def test_unknown_command():
     m = VendingMachine()
-    assert "알 수 없는 명령입니다" == m.run("아무거나")
+    assert "알 수 없는 명령입니다" == m.run("웅앵")
 
-def test_check_leftover_charge():
-    m = VendingMachine()
-    assert "잔액은 100원입니다" == m.run("잔액")
+# def test_valid_coins():
+#     m = VendingMachine()
+#     valid_coins = ["10", "50", "100", "500"]
+#     for coin in valid_coins:
+#         assert coin + "원을 넣었습니다" == m.run("동전 " + coin)
 
-def test_insert_coin_and_check():
+def test_인식_하는_동전():
     m = VendingMachine()
-    assert "100원을 넣었습니다" == m.run("동전 100")
-    assert "잔액은 100원입니다" == m.run("잔액")
+    valid_coins = ["10", "50", "100", "500"]
+    for coin in valid_coins:
+        assert coin + "원을 넣었습니다" == m.run("동전 " + coin)
+        # 동전 뒤에 스페이스 없으면 오류난다...주의!
 
-def test_enough_change():
+def test_인식할_수_없는_동전():
     m = VendingMachine()
-    assert "커피가 나왔습니다" == m.run("동전 100")
-
-def test_not_enough_change():
-    m = VendingMachine()
-    assert "잔액이 부족합니다" == m.run("동전 100")
+    assert "알 수 없는 동전입니다" == m.run("동전 110")
--- vm.py
+++ vm.py
@@ -2,26 +2,31 @@ class VendingMachine:
     def __init__(self):
         self._change = 0
 
-    def run(self,raw):
-        token = raw.split(" ")
-        cmd, params = token[0], token[1:]
+    def run(self, raw):
+        tokens = raw.split(" ")
+        cmd, params = tokens[0], tokens[1:]
 
         if cmd == "잔액":
             return "잔액은 " + str(self._change) + "원입니다"
-
         elif cmd == "동전":
-            coin = params[0]
-            self._change+= int(coin)
-            return coin + "원을 넣었습니다"
-
-        elif cmd =="음료":
-            if self._change >= 150:
-                self._change = self._change - 150
-                return "커피가 나왔습니다"
+            coin = str(params[0])
+            coin_list = ["10", "50", "100", "500"]
+            if coin in coin_list:
+                self._change += int(coin)
+                return coin + "원을 넣었습니다"
             else:
+                return "알 수 없는 동전입니다"
+
+        elif cmd == "음료":
+            known_beverage = "커피"
+            price = 150
+
+            beverage = params[0]
+            if beverage != known_beverage:
+                return "알 수 없는 음료입니다"
+            if self._change < price:
                 return "잔액이 부족합니다"
-            # coin = params[0]
-            # self._change += int(params[0])
-            # return coin + "원을 넣었습니다"
+            self._change = self._change - price
+            return beverage + "가 나왔습니다"
         else:
             return "알 수 없는 명령입니다"
